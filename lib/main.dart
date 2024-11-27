715b654ce446c10bb56f887b59a3197ae5d33b1c
ms82 <tachy0n82@wegocompany.net>
2024-11-27 15:02:34 +0900

    no message

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:amplify_api/amplify_api.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:my_amplify_app/models/ModelProvider.dart';
+import 'package:my_amplify_app/todo_screen.dart';
+
+import 'amplify_outputs.dart';
+
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await _configureAmplify();
+    runApp(const MyApp());
+  } on AmplifyException catch (e) {
+    runApp(Text("Error configuring Amplify: ${e.message}"));
+  }
+}
+
+Future<void> _configureAmplify() async {
+  try {
+    await Amplify.addPlugins(
+      [
+        AmplifyAuthCognito(),
+        AmplifyAPI(
+          options: APIPluginOptions(
+            modelProvider: ModelProvider.instance,
+          ),
+        ),
+      ],
+    );
+    await Amplify.configure(amplifyConfig);
+    safePrint('Successfully configured');
+  } on Exception catch (e) {
+    safePrint('Error configuring Amplify: $e');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Authenticator(
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        home: const SafeArea(
+          child: Scaffold(
+            body: Column(
+              children: [
+                SignOutButton(),
+                Expanded(child: TodoScreen()),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
