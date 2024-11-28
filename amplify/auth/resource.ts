03c995335abfe2627b2e4340f0061173fbbabe3e
ms82 <tachy0n82@wegocompany.net>
2024-11-28 10:51:04 +0900

    no message

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,7 +1,7 @@
 import { defineAuth } from '@aws-amplify/backend';
 import { addUserToGroup } from "../data/add-user-to-group/resource"
 /**
- * Define and configure your auth resource
+ * Define and configure your auth resource.
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
 export const auth = defineAuth({
